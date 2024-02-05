375b4f3ec542b983298b007d3199fbcd44259e7c
YuseinToslev <106616594+YusiToslev@users.noreply.github.com>
2024-02-05 17:39:06 +0200

    Update Program.cs

--- Sudoku/Sudoku/Program.cs
+++ Sudoku/Sudoku/Program.cs
@@ -4,18 +4,9 @@
     
     using System;
     using System.Collections.Generic;
-    internal class SudokuGame : methods
     internal class SudokuGame : Methods
     {
-        static void Main()
-        {
-            int[,] board = GenerateSudoku();
-            bool[,] userEntered = new bool[9, 9];
-            PlaySudoku(board, userEntered);
+       
 
-            // Wait for a key press before closing the console window
-            Console.ReadLine();
-        }
-
-          }
     }
+}
